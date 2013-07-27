c3ed985c7134b7659625e852e4eadd60f81f4d0f
Victor <vitimm144@gmail.com>
2013-07-26 21:30:35 -0300

    Esqueleto do projeto.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('IEPSApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
