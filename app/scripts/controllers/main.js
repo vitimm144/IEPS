557939344858c8f1eb0bef6da1075858396778b2
Victor <vitimm144@gmail.com>
2013-09-03 22:21:42 -0300

    Deletado api do projeto para nova instalação

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('IEPSApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    
+    $http.get('api/connect').success(function(){
+        console.log('conectou');
+    }).error(function(){
+        console.log('nao conectou');
+    });
+    $http.get('api/login').success(function(){
+        console.log('conectou');
+    }).error(function(){
+        console.log('nao conectou');
+    });
   });
