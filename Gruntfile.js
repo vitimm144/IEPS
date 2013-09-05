96b415a26066fd84b2a9fede4cb7d04637e384da
Victor <vitimm144@gmail.com>
2013-09-04 21:38:00 -0300

    Adicionado grunt-connect-proxy no projeto.

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
 };
+var proxySnippet = require('grunt-connect-proxy/lib/utils').proxyRequest;
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -74,10 +75,21 @@ module.exports = function (grunt) {
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0'
       },
+      proxies: [
+        {
+          context: '/api',
+          host: '127.0.0.1',
+          port: 8000,
+          https: false,
+          changeOrigin: false
+        }
+
+      ],
       livereload: {
         options: {
           middleware: function (connect) {
             return [
+              proxySnippet,
               lrSnippet,
               mountFolder(connect, '.tmp'),
               mountFolder(connect, yeomanConfig.app)
@@ -352,6 +364,7 @@ module.exports = function (grunt) {
     grunt.task.run([
       'clean:server',
       'concurrent:server',
+	  'configureProxies',
       'autoprefixer',
       'connect:livereload',
       'open',
