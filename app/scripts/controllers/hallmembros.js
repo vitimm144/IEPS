516a811e5485800c0944e642309a71c2a7e043d7
Victor <vitimm144@gmail.com>
2013-09-29 16:06:19 -0300

    Criação dos objetos para manipulação das informações.

--- app/scripts/controllers/hallmembros.js
+++ app/scripts/controllers/hallmembros.js
@@ -2,9 +2,52 @@
 
 angular.module('IEPSApp')
   .controller('HallmembrosCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.membro = {
+      nome : '',
+      rg : '',
+      profissao : '',
+      sexo : '',
+      tipo_sanguineo : '',
+      data_nascimento : new Date(),
+      nome_pai : '',
+      nome_mae : ''
+      
+    };
+    $scope.teologia = {
+      curso : '',
+      instituicao : '',
+      duracao : '',
+      anexo : ''
+    };
+    $scope.historico_familiar = {
+      estado_civil : '',
+      nome_conjuje : '',
+      data_casamento : new Date(),
+      filhos : false,
+      nr_filhos : 0
+    };
+    $scope.contato = {
+      residencial : '',
+      celular1 : '',
+      celular2 : '',
+      email : '',
+      facebook : ''
+    };
+    $scope.endereco = {
+      logradouro : '',
+      numero : '',
+      bairro : '',
+      complemento : '',
+      cep : ''
+    };
+    $scope.historico_eclesiastico = {
+      data_conversao : new Date(),
+      data_batismo : new Date()
+    };
+    $scope.cargo = {
+      cargo : '',
+      data_consagracao : new Date(),
+      igreja : '',
+      cidade : ''
+    };
   });
