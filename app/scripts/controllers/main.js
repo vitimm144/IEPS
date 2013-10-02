9578356d746e73682b10e4e67f37793eeaae635d
Victor <vitimm144@gmail.com>
2013-10-01 22:15:14 -0300

    -Comentando trecho de código desnecessário.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,14 +3,9 @@
 angular.module('IEPSApp')
   .controller('MainCtrl', function ($scope, $http) {
     
-    $http.get('api/connect').success(function(){
-        console.log('conectou');
-    }).error(function(){
-        console.log('nao conectou');
-    });
-    $http.get('api/login').success(function(){
-        console.log('conectou');
-    }).error(function(){
-        console.log('nao conectou');
-    });
+//    $http.get('api/login').success(function(){
+//        console.log('conectou');
+//    }).error(function(){
+//        console.log('nao conectou');
+//    });
   });
