053e1f8801a023099d8d6cb5355395f29e7128d7
Victor <vitimm144@gmail.com>
2013-10-28 23:51:52 -0200

    - Criado busca para as chaves estrangeiras da tabela de dados pessoais.

--- app/api/index.php
+++ app/api/index.php
@@ -6,7 +6,7 @@ require 'vendor/autoload.php';
 ini_set("display_errors", 1);
 
 session_start();
-
+date_default_timezone_set('UTC');
 use Respect\Rest\Router;
 
 $r3 = new Router('/api');
@@ -50,7 +50,6 @@ $r3->post('/cadastro', function(){
               .$data->cargo->igreja."','"
               .$data->cargo->cidade."');"
   );
-  
   $obj_conexao->query(
           "INSERT INTO historico_familiar(estado_civil, data_casamento, nome_conjuje, filhos, nr_filhos)
             VALUES('".$data->historico_familiar->estado_civil."','"
@@ -60,16 +59,17 @@ $r3->post('/cadastro', function(){
               .$data->historico_familiar->filhos.","
               .$data->historico_familiar->nr_filhos.");"
   );
+          echo $data->teologia->anexo;
   $obj_conexao->query(
           //TODO tratamento do tipo BLOB
-          "INSERT INTO teologia(curso, instituicao, duracao, anexos)
+          "INSERT INTO teologia(curso, instituicao, duracao)
             VALUES('".$data->teologia->curso."','"
               .$data->teologia->instituicao."','"
-              .$data->teologia->duracao."',"
-              .$data->teologia->anexo.");"
+              .$data->teologia->duracao."');"
+//              .$data->teologia->anexo.");"
   );
   try{
-    date_default_timezone_set('UTC');
+    
     $date = new DateTime( $data->cargo->data_consagracao );
     $id_cargo = $obj_conexao->query( 'SELECT id_cargo FROM cargo WHERE '
             . 'cargo="'.$data->cargo->cargo.'" AND '
@@ -87,11 +87,65 @@ $r3->post('/cadastro', function(){
         .$data->historico_eclesiastico->data_batismo."',"
         .$id_cargo_1.");"
   );
+  //buscando ids para inserir dados pessoais
+  try {
+    
+    $id_endereco = $obj_conexao->query(
+      'SELECT id_endereco FROM endereco WHERE '
+      . 'logradouro="'.$data->endereco->logradouro.'" AND '
+      . 'numero ="'.$data->endereco->numero.'" AND '
+      . 'bairro ="'.$data->endereco->bairro.'"'        
+            
+    );
+    $endereco = mysql_fetch_assoc( $id_endereco );
+    $id_endereco = $endereco['id_endereco'];
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
+
+  try {
+    $id_teologia = $obj_conexao->query(
+      'SELECT id_teologia FROM teologia WHERE '
+      . 'curso="'.$data->teologia->curso.'" AND '
+      . 'instituicao ="'.$data->teologia->instituicao.'" AND '
+      . 'duracao ="'.$data->teologia->duracao.'"'
+    );
+    $teologia = mysql_fetch_assoc( $id_teologia );
+    $id_teologia1 = $teologia['id_teologia'];
+
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
+  try {
+    $id_contato = $obj_conexao->query(
+      'SELECT id_contato FROM contato WHERE '
+      . 'residencial="'.$data->contato->residencial.'" OR '
+      . 'celular1="'.$data->contato->celular1.'"'
+    );
+    $contato = mysql_fetch_assoc( $id_contato );
+    $id_contato1 = $contato['id_contato'];
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
   
+  try {
+    $data_casamento = new DateTime( $data->historico_familiar->data_casamento );
+    $id_historico_familiar = $obj_conexao->query(
+      'SELECT id_historico_familiar FROM historico_familiar WHERE '
+      . 'estado_civil="'.$data->historico_familiar->estado_civil.'" AND '
+      . 'data_casamento ="'.$data_casamento->format( 'Y-m-d' ).'" AND '
+      . 'nome_conjuje="'.$data->historico_familiar->nome_conjuje.'"'
+    );
+    $historico_familiar = mysql_fetch_assoc( $id_historico_familiar);
+    $id_historico_familiar1 = $historico_familiar['id_historico_familiar'];
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
+
+
   $obj_conexao->desconectar();
-echo $data->teologia->anexo;
-return json_encode( $array['id_cargo'] );
-//  return $date;
+//  echo $data->teologia->anexo;
+return json_encode($historico_familiar['id_historico_familiar']);
 });
 
 
