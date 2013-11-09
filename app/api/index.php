841425e8c7e0b899524c2ecdd9843375c36021fe
Victor <vitimm144@gmail.com>
2013-11-08 23:21:56 -0200

    - Adicionado query de inserção de dados_pessoais.
    - Adicionado controle de erro de sql.

--- app/api/index.php
+++ app/api/index.php
@@ -27,7 +27,7 @@ $r3->post('/cadastro', function(){
   $data = json_decode(file_get_contents('php://input'));
   header('HTTP/1.1 200 Ok');
   //TODO ver questao de codificação de caracteres no banco !!!!!!
-  $obj_conexao->query(
+  $errorInsertContato =  $obj_conexao->query(
           "INSERT INTO contato(residencial, celular1, celular2, email, facebook)
             VALUES('".$data->contato->residencial."','"
               .$data->contato->celular1."','"
@@ -35,7 +35,11 @@ $r3->post('/cadastro', function(){
               .$data->contato->email."','"
               .$data->contato->facebook."');"
   );
-  $obj_conexao->query(
+  
+  if( $errorInsertContato == false ) {
+    echo 'erro em inserir contato';
+  }
+  $errorInsertEndereco = $obj_conexao->query(
           "INSERT INTO endereco(logradouro, numero, bairro, complemento, cep)
             VALUES('".$data->endereco->logradouro."','"
               .$data->endereco->numero."','"
@@ -43,14 +47,30 @@ $r3->post('/cadastro', function(){
               .$data->endereco->complemento."','"
               .$data->endereco->cep."');"
   );
-  $obj_conexao->query(
+  if( $errorInsertEndereco == false ) {
+    echo 'erro em inserir endereco';
+  }
+  
+  $errorInsertCargo = $obj_conexao->query(
           "INSERT INTO cargo(cargo, data_consagracao, igreja, cidade)
             VALUES('".$data->cargo->cargo."','"
               .$data->cargo->data_consagracao."','"
               .$data->cargo->igreja."','"
               .$data->cargo->cidade."');"
   );
-  $obj_conexao->query(
+  if( $errorInsertCargo == false ) {
+    echo 'erro em inserir cargo';
+  }
+  //verificar estado civil
+   
+  //Tratamento campo boolean pois o mysql não aceita tipo boolean
+  if ( $data->historico_familiar->filhos === true ) {
+    $data->historico_familiar->filhos = 1;
+  } else {
+    $data->historico_familiar->filhos = 0;
+  }
+  
+  $errorInsertHistFamiliar = $obj_conexao->query(
           "INSERT INTO historico_familiar(estado_civil, data_casamento, nome_conjuje, filhos, nr_filhos)
             VALUES('".$data->historico_familiar->estado_civil."','"
               .$data->historico_familiar->data_casamento."','"
@@ -59,8 +79,10 @@ $r3->post('/cadastro', function(){
               .$data->historico_familiar->filhos.","
               .$data->historico_familiar->nr_filhos.");"
   );
-          echo $data->teologia->anexo;
-  $obj_conexao->query(
+  if( $errorInsertHistFamiliar == false ) {
+    echo 'erro em inserir historico familiar';
+  }
+  $errorInsertTeologia = $obj_conexao->query(
           //TODO tratamento do tipo BLOB
           "INSERT INTO teologia(curso, instituicao, duracao)
             VALUES('".$data->teologia->curso."','"
@@ -68,6 +90,11 @@ $r3->post('/cadastro', function(){
               .$data->teologia->duracao."');"
 //              .$data->teologia->anexo.");"
   );
+  if( $errorInsertTeologia == false ) {
+    echo 'erro em inserir teologia';
+  }
+  
+  
   try{
     
     $date = new DateTime( $data->cargo->data_consagracao );
@@ -76,17 +103,20 @@ $r3->post('/cadastro', function(){
             . 'data_consagracao ="'.$date->format( 'Y-m-d' ).'" AND '
             . 'igreja="'.$data->cargo->igreja.'"' );
     $array = mysql_fetch_assoc( $id_cargo );
-    $nr_linhas = mysql_num_rows( $id_cargo );
     $id_cargo_1 = $array['id_cargo'];
   }  catch ( Exception $e ) {
     echo $e.'Erro em buscar chave estrangeira de cargo';
   }
-  $obj_conexao->query(
+  $errorInsertHistEcles = $obj_conexao->query(
     "INSERT INTO historico_eclesiastico(data_conversao, data_batismo, id_cargo)
       VALUES('".$data->historico_eclesiastico->data_conversao."','"
         .$data->historico_eclesiastico->data_batismo."',"
         .$id_cargo_1.");"
   );
+  
+  if( $errorInsertHistEcles == false || $id_cargo == false ) {
+    echo 'erro em inserir historico eclesiastico';
+  }
   //buscando ids para inserir dados pessoais
   try {
     
@@ -98,7 +128,7 @@ $r3->post('/cadastro', function(){
             
     );
     $endereco = mysql_fetch_assoc( $id_endereco );
-    $id_endereco = $endereco['id_endereco'];
+    $id_endereco1 = $endereco['id_endereco'];
   } catch (Exception $exc) {
     echo $exc->getTraceAsString();
   }
@@ -136,17 +166,67 @@ $r3->post('/cadastro', function(){
       . 'data_casamento ="'.$data_casamento->format( 'Y-m-d' ).'" AND '
       . 'nome_conjuje="'.$data->historico_familiar->nome_conjuje.'"'
     );
-    $historico_familiar = mysql_fetch_assoc( $id_historico_familiar);
+    $historico_familiar = mysql_fetch_assoc( $id_historico_familiar );
     $id_historico_familiar1 = $historico_familiar['id_historico_familiar'];
   } catch (Exception $exc) {
     echo $exc->getTraceAsString();
   }
-
-
+  
+  try {
+    $data_batismo = new DateTime($data->historico_eclesiastico->data_batismo);
+    $data_conversao = new DateTime($data->historico_eclesiastico->data_conversao);
+    $id_hist_eclesiastico = $obj_conexao->query(
+      'SELECT id_hist_eclesiastico FROM historico_eclesiastico WHERE '
+      . 'data_conversao ="'.$data_conversao->format( 'Y-m-d' ).'" AND '
+      . 'data_batismo ="'.$data_batismo->format( 'Y-m-d' ).'"'
+    );
+    $historico_eclesiastico = mysql_fetch_assoc( $id_hist_eclesiastico );
+    $id_hist_eclesiastico1 = $historico_eclesiastico['id_hist_eclesiastico'];
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
+  $data_nascimento = new DateTime($data->dados_pessoais->data_nascimento);
+  try {
+    $errorInsertDadosPessoais = $obj_conexao->query(
+      "INSERT INTO dados_pessoais(
+        nome,
+        rg,
+        sexo,
+        data_nascimento,
+        tipo_sanguineo,
+        nome_mae,
+        profissao,
+        nome_pai,
+        id_historico_familiar,
+        id_endereco,
+        id_contato,
+        id_hist_eclesiastico, 
+        id_teologia
+        ) VALUES( '"
+        .$data->dados_pessoais->nome."','"
+        .$data->dados_pessoais->rg."','"
+        .$data->dados_pessoais->sexo."','"
+        .$data_nascimento->format( 'Y-m-d' )."','"
+        .$data->dados_pessoais->tipo_sanguineo."','"
+        .$data->dados_pessoais->nome_mae."','"
+        .$data->dados_pessoais->profissao."','"
+        .$data->dados_pessoais->nome_pai."',"
+        .$id_historico_familiar1.","
+        .$id_endereco1.","
+        .$id_contato1.","
+        .$id_hist_eclesiastico1.","
+        .$id_teologia1.");"      
+    );
+    
+  } catch (Exception $exc) {
+    echo $exc->getTraceAsString();
+  }
+  if( $errorInsertDadosPessoais == false ) {
+    echo 'erro em inserir dados pessoais';
+  }
   $obj_conexao->desconectar();
-//  echo $data->teologia->anexo;
-return json_encode($historico_familiar['id_historico_familiar']);
-});
 
+return;
+});
 
 print $r3->run();
