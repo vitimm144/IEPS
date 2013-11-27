abda591a6a12fbc9f5a2fb1fd0334b7a7d2519ac
Victor <vitimm144@gmail.com>
2013-11-26 22:01:24 -0200

    - Adicionado uma requisição inicial para obter dados do servidor
    no inicio da view para listagem dos membros.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,27 @@ angular.module('IEPSApp', ['restangular','ngRoute'])
         templateUrl: 'views/cadmembros.html',
         controller: 'CadmembrosCtrl'
       })
+      .when('/listarmembros', {
+        templateUrl: 'views/listarmembros.html',
+        controller: 'ListarmembrosCtrl',
+        resolve:{
+//            Membros:[ '$http', '$q', function($http, $q){
+//             var deferred = $q.defer();
+//             $http.get('api/membros').success(function(data){
+//               deferred.resolve(data);
+//             });
+//             return deferred.promise;
+//            }]
+          Membros:[ 'Restangular', '$q', function(Restangular,$q){
+            var deferred = $q.defer();
+            Restangular.all('membros').getList().then(function(data){
+//              console.log(JSON.parse(data));
+              deferred.resolve(data);
+            });
+            return deferred.promise;
+          }]
+        }
+      })
       .otherwise({
         redirectTo: '/'
       });
