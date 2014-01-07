911dfabaff2bb1f898f293324b746c1a858e3c87
Victor <vitimm144@gmail.com>
2014-01-06 23:21:11 -0200

    - Utilizando o restangular para pegar a lista de membros.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,17 +16,9 @@ angular.module('IEPSApp', ['restangular','ngRoute'])
         templateUrl: 'views/listarmembros.html',
         controller: 'ListarmembrosCtrl',
         resolve:{
-//            Membros:[ '$http', '$q', function($http, $q){
-//             var deferred = $q.defer();
-//             $http.get('api/membros').success(function(data){
-//               deferred.resolve(data);
-//             });
-//             return deferred.promise;
-//            }]
           Membros:[ 'Restangular', '$q', function(Restangular,$q){
             var deferred = $q.defer();
             Restangular.all('membros').getList().then(function(data){
-//              console.log(JSON.parse(data));
               deferred.resolve(data);
             });
             return deferred.promise;
