3f5d78c18cebb62fddce4be8433a30df7b2cda27
victor <vitimm144@gmail.com>
2014-08-12 21:45:04 -0300

    Inserido configuração do postgres no projeto

--- backend/ieps/ieps/settings.py
+++ backend/ieps/ieps/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'ieps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ieps',
+        'USER': 'ieps',
+        'PASSWORD': 'ieps',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
