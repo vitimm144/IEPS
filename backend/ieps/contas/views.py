dd41ce2e05b0d6fe9baa97176385716784244cc5
victor <vitimm144@gmail.com>
2014-08-12 22:54:16 -0300

    Usuario pode listar contas

--- backend/ieps/contas/views.py
+++ backend/ieps/contas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+
+    model = User
+    serializer_class = UserSerializer
